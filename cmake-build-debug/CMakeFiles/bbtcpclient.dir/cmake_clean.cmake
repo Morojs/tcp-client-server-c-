67e794ccd387d51a77b8f6659a66767e25a68d26
Morojs <moro.hicham@emsi-edu.ma>
2020-05-03 01:32:30 +0000

    add tcp server example

---
+++ cmake-build-debug/CMakeFiles/bbtcpclient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bbtcpclient.dir/main.cpp.o"
+  "bbtcpclient"
+  "bbtcpclient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bbtcpclient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
